e50b45a67d2ff9e3ddbbe3b4f6fcb85b9a391e78
Rajveer Gandhi <rajveergandhi.cs@gmail.com>
2018-03-04 14:15:42 -0500

    added cost to link.java

--- src/main/java/socs/network/node/Link.java
+++ src/main/java/socs/network/node/Link.java
@@ -4,9 +4,11 @@ public class Link {
 
   RouterDescription router1;
   RouterDescription router2;
+  int weight;
 
-  public Link(RouterDescription r1, RouterDescription r2) {
+  public Link(RouterDescription r1, RouterDescription r2, int w) {
     router1 = r1;
     router2 = r2;
+    weight = w;
   }
 }
